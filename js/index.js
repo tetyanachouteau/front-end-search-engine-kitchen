0d380903f1cc008b0e8720b20c76099e254786b2
Tetyana <tetyana.chouteau@gmail.com>
2023-05-07 16:41:03 +0200

    Fin site static

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+const dropbtn = document.querySelectorAll(".dropbtn");
+dropbtn.forEach(el => {
+    // quand on click sur le faux select
+    el.addEventListener("click", (e) => {
+        const btn = e.currentTarget;
+        const div = document.querySelector("#" + btn.dataset.dropdown);
+        //on affiche et on met le focus sur le champs de recherche
+        div.style.display = "flex";
+        const input = div.getElementsByTagName("input")[0];
+        input.focus();
+    })
+})
 
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
+const dropinput = document.querySelectorAll(".dropdown input");
+dropinput.forEach(el => {
+    // recherche dans la liste des "options" (a)
+    el.addEventListener("keyup", (e) => {
+        const input = e.currentTarget;
+        const filter = input.value.toUpperCase();
+        const div = input.parentNode;
+        const a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+            let txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+            }
         }
-    }
-}
+    })
+    // quand on perd le focus on cache la div
+    // click sur un "option" ou click hors de la div
+    el.addEventListener("blur",(e) => {
+        const input = e.currentTarget;
+        const div = input.parentNode;
+        div.style.display="none";
+    })
+})
